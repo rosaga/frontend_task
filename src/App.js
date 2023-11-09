829be4bcf923da11e4848b3e4908c2db87a9f2e0
rosaga <richardjohnzranger@gmail.com>
2023-11-09 10:23:06 +0300

    update the dashboard url

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import { loadDashboards } from './utils/function';
 
 function App() {
   const [dashboards, setDashboards] = useState([]);
-  const [dashbardURL, setDashboardUrl] = useState('https://gist.githubusercontent.com/kabaros/da79636249e10a7c991a4638205b1726/raw/fa044f54e7a5493b06bb51da40ecc3a9cb4cd3a5/dashboards.json')
+  const dashbardURL = useState('https://gist.githubusercontent.com/kabaros/da79636249e10a7c991a4638205b1726/raw/fa044f54e7a5493b06bb51da40ecc3a9cb4cd3a5/dashboards.json')
 
   const getDashboards = async () => {
     try {
