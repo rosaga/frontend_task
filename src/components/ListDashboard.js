701cadbdb9c4cb74ea3d3dbb0061de907cfe4341
rosaga <richardjohnzranger@gmail.com>
2023-11-10 13:23:23 +0300

    minor edits to handle dashboards urls as variables

--- src/components/ListDashboard.js
+++ src/components/ListDashboard.js
@@ -16,7 +16,7 @@ const ListDashboard = ({ dashboards }) => {
 
     const handleCardClick = (id) => {
         setDashboardId(id);
-        setDashboardUrl(`https://gist.githubusercontent.com/kabaros/da79636249e10a7c991a4638205b1726/raw/fa044f54e7a5493b06bb51da40ecc3a9cb4cd3a5/${id}.json`);
+        setDashboardUrl(baseUrl+`${id}.json`);
 
         setExpandedDashboards((prevExpandedDashboards) => ({
             // ...prevExpandedDashboards,
